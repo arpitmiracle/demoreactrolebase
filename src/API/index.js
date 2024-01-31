b68c16a0293dfe230eb5b4d6bd009bffbc6ec5f1
arpitmiracle <113773212+arpitmiracle@users.noreply.github.com>
2024-01-31 13:37:56 +0530

    add

--- src/API/index.js
+++ src/API/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL
     ? process.env.REACT_APP_API_URL
-    : "http://localhost:4000",
+    : "https://f3e6-2405-201-200c-701a-381a-389f-1e4b-c873.ngrok-free.app",
 });
 
 API.interceptors.request.use(
